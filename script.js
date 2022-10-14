60e4d6ddfc5a70453e791971026aee9ccdccb45b
Dallas Palumbo <palumbodallas@gmail.com>
2022-10-13 22:00:33 -0400

    getting responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+const arrow = document.querySelector("#arrow");
+const heading = document.querySelector("#heading")
+
+
+
+heading.addEventListener("mouseover",()=>{
+    arrow.getElementsByClassName.transform = "translate(10%)"
+})
+
+console.log(arrow)
